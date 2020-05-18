a749fd7d0193f4bef83029d2daecdf68ba4e1148
jsilvaroland <joshuasilvaroland@gmail.com>
2020-05-18 14:57:01 -0700

    Add deck repopulate and shuffle logic

---
+++ src/deck.js
@@ -0,0 +1,61 @@
+const ATTRIBUTES = {
+	colors: ['red', 'green', 'purple'],
+	numbers: ['one', 'two', 'three'],
+	shapes: ['oval', 'squiggle', 'diamond'],
+	shadings: ['solid', 'striped', 'open'],
+};
+
+export default class Deck {
+	constructor() {
+		this.deck = [];
+	}
+
+	repopulateDeckNovice() {
+	// empties deck if not already empty
+		this.deck = [];
+
+		ATTRIBUTES.colors.forEach(color => {
+			ATTRIBUTES.numbers.forEach(number => {
+				ATTRIBUTES.shapes.forEach(shape => {
+					this.deck.push({ color, number, shape, shading: 'solid' });
+				});
+			});
+		});
+	}
+
+	repopulateDeckExpert() {
+		// empties deck if not already empty
+		this.deck = [];
+		
+		ATTRIBUTES.colors.forEach(color => {
+			ATTRIBUTES.numbers.forEach(number => {
+				ATTRIBUTES.shapes.forEach(shape => {
+					ATTRIBUTES.shadings.forEach(shading => {
+						this.deck.push({ color, number, shape, shading });
+					});
+				});
+			});
+		});
+	}
+
+	shuffle() {
+		const { deck } = this;
+		let count = deck.length;
+
+		while (count) {
+			let i = Math.floor(Math.random() * count);
+			[deck[count], deck[i]] = [deck[i], deck[count]];
+			count--;
+		}
+	}
+
+	resetDeckNovice() {
+		this.repopulateDeckNovice();
+		this.shuffle();
+	}
+
+	resetDeckExpert() {
+		this.repopulateDeckExpert();
+		this.shuffle();
+	}
+}
