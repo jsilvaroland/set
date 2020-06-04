540876d5b0dc0f10c5e9ec57d776df244f923e6e
jsilvaroland <joshuasilvaroland@gmail.com>
2020-06-04 16:19:48 -0700

    Add instructions modal

--- src/javascripts/bundle.js
+++ src/javascripts/bundle.js
@@ -475,10 +475,39 @@ __webpack_require__.r(__webpack_exports__);
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Modal logic
+  const modal = document.getElementById("instructions-modal");
+  const modalPage2 = document.getElementById("instructions-modal-page-2");
+  const instructionsBtn = document.getElementById("instructions-btn");
+  const closeModalX = document.getElementsByClassName("close")[0];
+  const modalNext = document.getElementsByClassName("next")[0];
+
+  instructionsBtn.onclick = () => {
+    modal.style.display = "block";
+  };
+
+  closeModalX.onclick = () => {
+    modal.style.display = "none";
+    modalPage2.style.display = "none";
+  };
+
+  modalNext.onclick = () => {
+    modal.style.display = "none";
+    modalPage2.style.display = "block";
+  };
+
+  window.onclick = (e) => {
+    if (e.target == modal || e.target == modalPage2) {
+      modal.style.display = "none";
+      modalPage2.style.display = "none";
+    }
+  };
+
+  // Canvas
   const canvas = document.getElementById("set-game");
   const set = new _set__WEBPACK_IMPORTED_MODULE_0__["default"](canvas);
-  // loads page with novice difficulty game
-  set.newGameNovice();
+  
+  set.newGameNovice(); // loads page with novice difficulty game
   
   const newGameNovice = document.getElementById("new-game-novice");
   newGameNovice.addEventListener("click", () => {
--- src/javascripts/entry.js
+++ src/javascripts/entry.js
@@ -1,10 +1,39 @@
 import Set from './set';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Modal logic
+  const modal = document.getElementById("instructions-modal");
+  const modalPage2 = document.getElementById("instructions-modal-page-2");
+  const instructionsBtn = document.getElementById("instructions-btn");
+  const closeModalX = document.getElementsByClassName("close")[0];
+  const modalNext = document.getElementsByClassName("next")[0];
+
+  instructionsBtn.onclick = () => {
+    modal.style.display = "block";
+  };
+
+  closeModalX.onclick = () => {
+    modal.style.display = "none";
+    modalPage2.style.display = "none";
+  };
+
+  modalNext.onclick = () => {
+    modal.style.display = "none";
+    modalPage2.style.display = "block";
+  };
+
+  window.onclick = (e) => {
+    if (e.target == modal || e.target == modalPage2) {
+      modal.style.display = "none";
+      modalPage2.style.display = "none";
+    }
+  };
+
+  // Canvas
   const canvas = document.getElementById("set-game");
   const set = new Set(canvas);
-  // loads page with novice difficulty game
-  set.newGameNovice();
+  
+  set.newGameNovice(); // loads page with novice difficulty game
   
   const newGameNovice = document.getElementById("new-game-novice");
   newGameNovice.addEventListener("click", () => {
